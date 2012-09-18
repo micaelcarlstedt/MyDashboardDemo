3a674e897b65e9c8fd32cc4a7b11b7f2bee54dd8
Micael Carlstedt <micael@carlstedt.com>
2012-09-19 00:25:31 +0200

    Take a picture

--- MyDashboardDemo.App/MainPage.xaml.cs
+++ MyDashboardDemo.App/MainPage.xaml.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
@@ -21,9 +28,14 @@ namespace MyDashboardDemo.App
     /// </summary>
     public sealed partial class MainPage : MyDashboardDemo.App.Common.LayoutAwarePage
     {
+        private IPropertySet appSettings;
+        private const string userNameKey = "username";
+        private const string photoKey = "photo";
+        
         public MainPage()
         {
             this.InitializeComponent();
+            appSettings = ApplicationData.Current.RoamingSettings.Values;
         }
 
         /// <summary>
@@ -35,19 +47,25 @@ namespace MyDashboardDemo.App
         /// </param>
         /// <param name="pageState">A dictionary of state preserved by this page during an earlier
         /// session.  This will be null the first time a page is visited.</param>
-        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        protected async override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
         {
-            // TODO: Restore session state
             if (pageState != null && pageState.ContainsKey("greetingOutputText"))
             {
                 greetingOutput.Text = pageState["greetingOutputText"].ToString();
             }
 
-            // TODO: Restore app data
-            Windows.Storage.ApplicationDataContainer roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
-            if (roamingSettings.Values.ContainsKey("userName"))
+            if (appSettings.ContainsKey(userNameKey))
             {
-                nameInput.Text = roamingSettings.Values["userName"].ToString();
+                nameInput.Text = appSettings[userNameKey].ToString();
+            }
+
+            if (appSettings.ContainsKey(photoKey))
+            {
+                object filePath;
+                if (appSettings.TryGetValue(photoKey, out filePath) && filePath.ToString() != "")
+                {
+                    await ReloadPhoto(filePath.ToString());
+                }
             }
         }
 
@@ -70,9 +88,73 @@ namespace MyDashboardDemo.App
         private void nameInput_TextChanged(object sender, TextChangedEventArgs e)
         {
             // Our applicationdata needs to be stored as it changes.
-            Windows.Storage.ApplicationDataContainer roamingSettings =
-                Windows.Storage.ApplicationData.Current.RoamingSettings;
-            roamingSettings.Values["userName"] = nameInput.Text;
+            appSettings[userNameKey] = nameInput.Text;
+        }
+
+        private void StartPreview(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private async void SnapPicture(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                // Using Windows.Media.Capture.CameraCaptureUI API to capture a photo
+                CameraCaptureUI dialog = new CameraCaptureUI();
+                Size aspectRatio = new Size(16, 9);
+                dialog.PhotoSettings.CroppedAspectRatio = aspectRatio;
+
+                StorageFile file = await dialog.CaptureFileAsync(CameraCaptureUIMode.Photo);
+                if (file != null)
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    using (IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read))
+                    {
+                        bitmapImage.SetSource(fileStream);
+                    }
+                    CapturedPhoto.Source = bitmapImage;
+
+                    // TODO: Store the file path in Application Data
+                    appSettings[photoKey] = file.Path;
+                }
+                else
+                {
+                    NotifyUser("No photo taken.");
+                }
+            }
+            catch (Exception ex)
+            {
+                NotifyUser(ex.Message);
+            }
+        }
+
+        private async Task ReloadPhoto(String filePath)
+        {
+            try
+            {
+                StorageFile file = await StorageFile.GetFileFromPathAsync(filePath);
+                BitmapImage bitmapImage = new BitmapImage();
+                using (IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read))
+                {
+                    bitmapImage.SetSource(fileStream);
+                }
+                CapturedPhoto.Source = bitmapImage;
+            }
+            catch (Exception ex)
+            {
+                appSettings.Remove(photoKey);
+                NotifyUser(ex.Message);
+            }
+        }
+
+        private async void NotifyUser(string message)
+        {
+            MessageDialog md = new MessageDialog(message, "Information");
+            
+            //md.Commands.Add( new UICommand("OK", new UICommandInvokedHandler((cmd) => result = true)));
+            
+            await md.ShowAsync();
 
         }
     }
